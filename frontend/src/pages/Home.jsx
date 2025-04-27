849d6ac710282efcc436046248d369a6d233f6c4
lanseylu <100890425+lanseylu@users.noreply.github.com>
2025-04-27 23:06:02 +0800

    Cleaned up

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 import { AiOutlineEdit } from 'react-icons/ai'
 import { BsInfoCircle } from 'react-icons/bs'
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
-import BooksTable from  '../components/home/BooksTable.jsx';
-import BooksCard from '../components/home/BooksCard.jsx';
+// import BooksTable from  '../components/home/BooksTable.jsx';
+// import BooksCard from '../components/home/BooksCard.jsx';
 
 const Home = () => {
   const [books, setBooks] = useState([]);
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -23,13 +23,13 @@ const ShowBook = () => {
       });
   }, [])
   return (
-    <div>
+    <div className='p-4'>
       <BackButton></BackButton>
         <h1 className='text-3xl my-4'>Show Book</h1>
         {loading ? (
           <Spinner />
         ) : (
-          <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+          <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
             <div className="my-4">
               <span className='text-xl mr-4 text-gray-500'>ID</span>
               <span>{book._id}</span>
